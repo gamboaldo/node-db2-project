b6ad64b8b233cc752b505a50ad409f4b48a0de0b
Aldo Mata <gamboaldo@gmail.com>
2021-07-13 09:57:29 -0700

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -2,14 +2,19 @@ const db = require("../../data/db-config");
 
 const getAll = () => {
   // DO YOUR MAGIC
+  return db("cars");
 };
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
+  return db("cars").where({ id }).first();
 };
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const [newCar] = await db("cars").insert(car);
+  const foo = await getById(newCar);
+  return foo;
 };
 
 const getVinNumber = (vin) => {
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -9,6 +9,36 @@ const {
   checkVinNumberUnique,
 } = require("./cars-middleware");
 
+router.get("/", (req, res, next) => {
+  Cars.getAll().then((cars) => {
+    res.status(200).json(cars);
+  });
+});
+
+router.get("/:id", checkCarId, (req, res, next) => {
+  res.status(200).json(req.car);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    //hello
+    const newCar = {
+      vin: req.body.vin,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      title: req.body.title,
+      transmission: req.body.transmission,
+    };
+    const car = await Cars.create(newCar);
+    res.status(200).json(car);
+  }
+);
+
 router.use((err, req, res) => {
   // eslint-disable-line
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
